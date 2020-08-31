e3d765bc20919a5b751dba5ae272f57c0ebe24df
Shubham Dhumal <54709753+ShadyD45@users.noreply.github.com>
2020-08-31 11:46:43 +0530

    Add files via upload

---
+++ Level1/foo1.java
@@ -0,0 +1,51 @@
+class Solution 
+{
+    public static String solution(String x) 
+    {
+        // Your code here
+        int i, j = 25;
+        char[] sent = x.toCharArray();
+        String decipheredStr = "";
+        final String alphabets = "abcdefghijklmnopqrstuvwxyz";
+        
+        for(char s : sent )
+        {
+            if(Character.isLowerCase(s))
+            {
+                decipheredStr += alphabets.charAt(getLetterPosition(s) + 1);
+            }
+            else
+            {
+                decipheredStr += s;
+            }
+        }
+        
+        return decipheredStr;
+    }
+    
+    private static int getLetterPosition(char ch)
+    {
+        final String alpha = "zyxwvutsrqponmlkjihgfedcba ";
+        int res = 0;
+        for(int i=0;i<alpha.length();i++)
+        {
+            if(alpha.charAt(i) == ch)
+            {
+                res = i;
+        		break;
+        	}
+        }
+        return res;
+    }
+}
+
+public class foo1
+{
+	public static void main(String args[])
+	{
+		Solution obj = new Solution();
+		
+		System.out.println(obj.solution("Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"));
+		
+	}
+}
